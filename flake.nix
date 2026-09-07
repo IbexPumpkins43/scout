09579e7a384cfae880bc13ff245d11f7fa984631
IbexPumpkins43 <283789752+IbexPumpkins43@users.noreply.github.com>
2026-09-07 07:09:33 +0100

    Add a nix flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          dotnetCorePackages.sdk_10_0
+        ];
+
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+          #pkgs.raylib
+          #pkgs.glfw
+          #pkgs.libGL
+          pkgs.xorg.libX11
+          #pkgs.xorg.libXrandr
+          #pkgs.xorg.libXi
+          #pkgs.xorg.libXcursor
+          #pkgs.xorg.libXinerama
+        ];
+      };
+    };
+}
